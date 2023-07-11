a6ddee1b473c77bd51e98cb35bf2403ec9695e0b
Tasneem-Alabed <tsneemabedd@gmail.com>
2023-07-11 06:00:08 +0300

    done

---
+++ Read_Json/Read_Json/Program.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Read_Json
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string File_json_path = "../../../../data.json";
+
+            string data_json = File.ReadAllText(File_json_path);
+            var Object = JsonConvert.DeserializeObject<point>(data_json);
+            IEnumerable<string> neighborhoods = Object.features.Select(fitter => fitter.properties.neighborhood);
+
+            Console.WriteLine("Neighborhoods:");
+            Console.WriteLine(string.Join("\t", neighborhoods));
+            Console.WriteLine("Total: " + neighborhoods.Count() + " neighborhoods");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("----------------------------------------");
+            var namedNeighborhoods = neighborhoods.Where(n => !string.IsNullOrEmpty(n));
+            Console.WriteLine("Named neighborhoods:");
+            Console.WriteLine(string.Join("\t", namedNeighborhoods));
+            Console.WriteLine("Total " + namedNeighborhoods.Count() + " neighborhoods");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("----------------------------------------");
+            var distinctNeighborhoods = namedNeighborhoods.Distinct();
+            Console.WriteLine("Distinct neighborhoods:");
+            Console.WriteLine(string.Join("\t", distinctNeighborhoods));
+            Console.WriteLine("Total " + distinctNeighborhoods.Count() + " neighborhoods");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("----------------------------------------");
+            var datedNeighborhoods = Object.features
+                .Select(f => f.properties.neighborhood)
+                .Where(n => !string.IsNullOrEmpty(n))
+                .Distinct();
+            Console.WriteLine("Consolidated neighborhoods:");
+            Console.WriteLine(string.Join("\t ", datedNeighborhoods));
+            Console.WriteLine("Total " + datedNeighborhoods.Count() + " neighborhoods");
+
+
+            var Data_Neighborhoods_Querys = (from f in Object.features
+                                           let n = f.properties.neighborhood
+                                           where !string.IsNullOrEmpty(n)
+                                           select n).Distinct();
+            Console.WriteLine("Consolidated neighborhoods (using query syntax):");
+            Console.WriteLine(string.Join("\t ", Data_Neighborhoods_Querys));
+
+            Console.WriteLine("Total " + Data_Neighborhoods_Querys.Count() + " neighborhoods");
+
+
+           
+        }
+
+    }
+    }
+
+    public class Feature
+    {
+        public string type { get; set; }
+        public Geometry geometry { get; set; }
+        public Properties properties { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string type { get; set; }
+        public List<double> list { get; set; }
+    }
+
+    public class Properties
+    {
+        public string zip { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string address { get; set; }
+        public string borough { get; set; }
+        public string neighborhood { get; set; }
+        public string county { get; set; }
+    }
+
+    public class point
+    {
+        public string type { get; set; }
+        public List<Feature> features { get; set; }
+    }
+
